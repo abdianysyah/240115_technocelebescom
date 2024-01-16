0055ab064e8817344ae59ea8cfb3bb8df7cda14a
juniarahma <junia8552@gmail.com>
2024-01-16 12:05:50 +0800

    buat navbar

---
+++ resources/views/master/master_home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('theme/home/plugin/bootstrap-5.0.2-dist/css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
+</head>
+<body>
+    @include('layout.home.header')
+    @yield('content')
+    @include('layout.home.footer')
+</body>
+</html>

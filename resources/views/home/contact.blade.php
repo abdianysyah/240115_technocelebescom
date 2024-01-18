813be3cfef01ccd274a6a99d908cc35890771013
AmyQadri <qodristr@gmail.com>
2024-01-18 18:22:18 +0800

    update bread Crumb

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,41 +1,54 @@
 @extends('master.master_inner')
 @section('content')
-<main class="vh-100">
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-lg-6 p-3">
-                <div class="card shadow p-5">
-                    <div class="card-body p-5">
-                        <h2 class="card-title text-center fw-bolder p-3">Contact Us</h2>
-                        <form action="" method="">
-                            <div class="row">
-                                <div class="col-md">
-                                  <div class="form-floating mb-2">
-                                    <input type="text" class="form-control" placeholder="full name">
-                                    <label for="floatingInputGrid">Nama</label>
-                                  </div>
-                                </div>
-                                <div class="col-md">
-                                    <div class="form-floating mb-2">
-                                        <input type="text" class="form-control" placeholder="email address" >
-                                        <label for="floatingInputGrid">Email</label>
+<main>
+    <!-- bread-crumb -->
+    <div class="bread-crumb d-flex align-items-center w-100" style="height: 20rem">
+        <div class="container py-4 text-center" data-aos="fade-up" data-aos-duration="0900">
+            <h1 class="display-2 fw-bold">Contact</h1>
+            <ul class="text-breadcrumb list-unstyled d-flex fs-4 justify-content-center align-items-center">
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li class="mx-2">|</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+    <div class="my-5">
+        <div class="container-fluid" data-aos="zoom-in" data-aos-duration="1200">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 p-3">
+                    <div class="card shadow p-5">
+                        <div class="card-body p-5">
+                            <h2 class="card-title text-center fw-bolder p-3">Contact Us</h2>
+                            <form action="" method="">
+                                <div class="row">
+                                    <div class="col-md">
+                                        <div class="form-floating mb-2">
+                                            <input type="text" class="form-control" placeholder="full name">
+                                            <label for="floatingInputGrid">Nama</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md">
+                                        <div class="form-floating mb-2">
+                                            <input type="text" class="form-control" placeholder="email address">
+                                            <label for="floatingInputGrid">Email</label>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" placeholder="subject">
-                                <label for="floatingSubject">Subject</label>
-                            </div>
-                                  
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" placeholder="massage" style="height: 100px"></textarea>
-                                <label for="floatingPassword">Pesan</label>
-                            </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" placeholder="subject">
+                                    <label for="floatingSubject">Subject</label>
+                                </div>
 
-                            <div class="d-grid gap-2">
-                                <button type="submit" class="btn btn-primary">Kirim</button>
-                            </div>
-                        </form>
+                                <div class="form-floating mb-3">
+                                    <textarea class="form-control" placeholder="massage" style="height: 100px"></textarea>
+                                    <label for="floatingPassword">Pesan</label>
+                                </div>
+
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Kirim</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/master/master_inner.blade.php
+++ resources/views/master/master_inner.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Techno Celebes || Jasa Teknologi dan Informasi Kota Palu</title>
     <link rel="stylesheet" href="{{asset('theme/home/plugin/bootstrap-5.0.2-dist/css/bootstrap.min.css')}}" />
     <link rel="stylesheet" href="{{asset('theme/home/plugin/font-awesome-4.7.0/css/font-awesome.min.css')}}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">

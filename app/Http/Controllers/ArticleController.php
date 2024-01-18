fb935b1a48ddeb88117c6e161d818c645cd571b4
ArhanRamadan <arhanramadhan243@gmail.com>
2024-01-18 14:58:25 +0700

    back-end

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -12,15 +12,15 @@ class ArticleController extends Controller
         $article = ArticleModel::all();
         return view('article',['article' => $article]);
     }
-    public function create_stock(Request $request){
-        $nama_barang = $request->namabarang;
-        $harga_jual = $request->hargajual;
-        $harga_beli = $request->hargabeli;
+    public function create_article(Request $request){
+        $title = $request->title;
+        $image = $request->image;
+        $isi = $request->isi;
 
         $k = new ArticleModel();
-        $k->nama_barang = $nama_barang;
-        $k->harga_beli = $harga_beli;
-        $k->harga_jual = $harga_jual;
+        $k->title = $title;
+        $k->image = $image;
+        $k->isi = $isi;
         $k->save();
         return redirect()->route('create-stock-get');
     }

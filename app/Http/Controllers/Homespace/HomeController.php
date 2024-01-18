d9f3950ff77aa7d18e94b1836d175bdfb67e5ab7
ArhanRamadan <arhanramadhan243@gmail.com>
2024-01-18 13:19:18 +0700

    back-end

--- app/Http/Controllers/Homespace/HomeController.php
+++ app/Http/Controllers/Homespace/HomeController.php
@@ -1,27 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\HomeSpace;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\Service as ServiceModel;
 
 class HomeController extends Controller
 {
-    /**
-    * Create a new controller instance.
-    *
-    * @return void
-    */
-    public function __construct()
-    {
-         $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('home.home');
@@ -46,5 +31,4 @@ class HomeController extends Controller
     {
         return view('home.visi&misi');
     }
-
 }

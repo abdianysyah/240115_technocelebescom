e9d298596046d028c7d1cc84930afa8f7b181ae7
AmyQadri <qodristr@gmail.com>
2024-01-24 13:50:51 +0800

    update drag and drop

--- resources/views/panel/article/create.blade.php
+++ resources/views/panel/article/create.blade.php
@@ -7,12 +7,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard v2</h1>
+                    <h1 class="m-0">Article Create</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v2</li>
+                        <li class="breadcrumb-item active">Article Create</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -27,32 +27,77 @@
                 <div class="row">
                     <div class="col-lg-8">
                         <div class="form-floating my-3">
-                          <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
-                          <label for="floatingInput">Judul Article</label>
+                            <input type="text" class="form-control" id="floatingInput" placeholder="Judul">
+                            <label for="floatingInput">Judul Article</label>
                         </div>
-                        <div class="input-group mb-3">
-                          <label class="input-group-text" for="inputGroupFile01">Upload</label>
-                          <input type="file" class="form-control" id="inputGroupFile01" placeholder="masukkan foto">
+                        <div class="drop-area text-center" ondrop="dropHandler(event)" ondragover="dragOverHandler(event)" style="background-color: #fff;border:2px solid #000;">
+                            <p class="mb-3">Drag & drop an image here</p>
+                            <input type="file" id="file-input" accept="image/*" onchange="handleFileSelect(event)" class="d-none">
+                            <label for="file-input" class="btn btn-primary">Choose
+                                File</label>
+                            <img id="preview-image" src="#" alt="Preview" style="display:none; max-width: 100%; margin-top: 20px;">
                         </div>
-                        <div class="form-floating my-3">
-                          <textarea name="content" id="editor1" rows="5" cols="80" ></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
+
+                    <!-- text area di sini -->
                 </div>
             </div>
-
-            <!-- Textarea yang akan digantikan oleh CKEditor -->
-            <textarea name="content"></textarea>
-            <!-- Tombol untuk memanggil fungsi getValueFromCKEditor() -->
-            <button onclick="getValueFromCKEditor()">Dapatkan Nilai CKEditor</button>
-
         </div>
-    </section>
-    <!-- /.content -->
+</div>
+</section>
+<!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 
 
 
+@endsection
+
+@section('drag-img')
+<script type="text/javascript">
+    function dropHandler(event) {
+        event.preventDefault();
+
+        if (event.dataTransfer.items) {
+            for (let i = 0; i < event.dataTransfer.items.length; i++) {
+                if (event.dataTransfer.items[i].kind === 'file') {
+                    const file = event.dataTransfer.items[i].getAsFile();
+                    displayImage(file);
+                }
+            }
+        } else {
+            for (let i = 0; i < event.dataTransfer.files.length; i++) {
+                displayImage(event.dataTransfer.files[i]);
+            }
+        }
+    }
+
+    function dragOverHandler(event) {
+        event.preventDefault();
+    }
+
+    function handleFileSelect(event) {
+        const file = event.target.files[0];
+        displayImage(file);
+    }
+
+    function displayImage(file) {
+        const previewImage = document.getElementById('preview-image');
+        const dropArea = document.querySelector('.drop-area');
+
+        if (file && file.type.startsWith('image/')) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                previewImage.src = e.target.result;
+                previewImage.style.display = 'block';
+                dropArea.classList.add('border-success');
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            alert('Invalid file format. Please choose an image.');
+        }
+    }
+</script>
 @endsection
